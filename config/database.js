8b762d0106a11dbfea4c35cf3cc6b4d52090ba5f
DevDHera <devdhera@gmail.com>
2018-04-08 00:09:39 +0530

    Initial Commit

---
+++ config/database.js
@@ -0,0 +1,9 @@
+if(process.env.NODE_ENV === 'production'){
+    module.exports = {
+        mongoURI: 'mongodb://devd:devd12312@ds237979.mlab.com:37979/vidjot-prod'
+    }
+}else{
+    module.exports = {
+        mongoURI: 'mongodb://localhost/vidjot-dev'
+    }
+}
